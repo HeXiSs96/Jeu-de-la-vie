3601b43e2030fc0037be250f45e58f1740913aa0
Angy-prt <angelique.porte64@gmail.com>
2024-12-08 13:56:58 +0100

    Update Cell.hpp

--- inc/Cell.hpp
+++ inc/Cell.hpp
@@ -6,7 +6,7 @@
 
 class Cell {
 public:
-    virtual void update(int x, int y, const std::vector<std::vector<Cell*>>& grid) = 0;
+    virtual void update(int x, int y, std::vector<std::vector<Cell*>>& grid) = 0;
     virtual void render(int x, int y, sf::RenderWindow& window, int cellSize) = 0;
     virtual ~Cell() = default;  // Destructeur virtuel pour garantir la destruction correcte des objets dérivés
 };
